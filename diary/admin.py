2f4f28ec02932c1f8bb45d2814a5d89266108639
raylicola <raylicola@gmail.com>
2022-02-23 19:58:26 +0900

    モデルの作成

--- diary/admin.py
+++ diary/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
 
+from diary.models import Diary, User
+
 # Register your models here.
+admin.site.register(User)
+admin.site.register(Diary)
--- diary/models.py
+++ diary/models.py
@@ -1,3 +1,17 @@
+import email
 from django.db import models
 
 # Create your models here.
+class User(models.Model):
+    email = models.EmailField(max_length=100)
+    password = models.CharField(max_length=100)
+    def __str__(self):
+        return self.email
+
+class Diary(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    tension = models.IntegerField()
+    detail = models.CharField(max_length=200)
+    date = models.DateField()
+    def __str__(self):
+        return str(self.user.email) + '(' + str(self.date) + ')'
