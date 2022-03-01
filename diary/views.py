a33f01bf8818e0d895b7cc0abff53ef9294c2cb6
raylicola <raylicola@gmail.com>
2022-03-01 21:57:55 +0900

    create graph display

--- diary/views.py
+++ diary/views.py
@@ -1,6 +1,15 @@
+from turtle import color
 from django.shortcuts import redirect, render
 
 from diary.forms import DiaryForm, LoginUserForm, RegisterUserForm, SettingEmailForm, SettingPasswordForm
+import matplotlib
+matplotlib.use('Agg')
+import matplotlib.pyplot as plt
+import base64, io
+import numpy as np
+
+figsize_x = 12
+figsize_y = 4
 
 # Create your views here.
 '''
@@ -8,10 +17,37 @@ from diary.forms import DiaryForm, LoginUserForm, RegisterUserForm, SettingEmail
   * 日記の新規作成
 '''
 def index(request):
+    x = list(range(-5,5))
+    y = list(range(len(x)))
+    create_graph(x, y)
+    tension_graph = get_image()
     params = {
         'title': 'Tensionary',
+        'tension_graph': tension_graph,
     }
     return render(request, 'diary/index.html', params)
+
+
+def create_graph(x, y):
+    plt.cla()
+    plt.figure(figsize=(figsize_x,figsize_y))
+    ax = plt.gca()
+    ax.axes.yaxis.set_visible(False)
+    ax.plot(x,y, 'k-', lw=1.5, alpha=0.6, color='red',label='')
+    ax.plot(x, y, 'o', color='red', markersize=5, markeredgewidth=3,
+        markeredgecolor='red', alpha=0.8, label='')
+
+
+
+def get_image():
+    buffer = io.BytesIO()
+    plt.savefig(buffer, format='png')
+    image_png = buffer.getvalue()
+    graph = base64.b64encode(image_png)
+    graph = graph.decode('utf-8')
+    buffer.close()
+    return graph
+
 
 '''
 日記新規作成
@@ -27,6 +63,7 @@ def create_diary(request):
         return redirect(to='/diary')
     return render(request, 'diary/create_diary.html', params)
 
+
 '''
 日記編集
 '''
@@ -40,6 +77,7 @@ def edit_diary(request, num):
         return redirect(to='/diary')
     return render(request, 'diary/edit_diary.html', params)
 
+
 '''
 設定画面
 '''
@@ -49,6 +87,7 @@ def setting(request):
     }
     return render(request, 'diary/setting.html', params)
 
+
 def setting_email(request):
     params = {
         'title': 'Setting Password',
@@ -56,12 +95,14 @@ def setting_email(request):
     }
     return render(request, 'diary/setting_email.html', params)
 
+
 def setting_password(request):
     params = {
         'title': 'Setting Password',
         'form': SettingPasswordForm(),
     }
     return render(request, 'diary/setting_password.html', params)
+
 
 '''
 ログイン
@@ -77,6 +118,7 @@ def login(request):
         'form': LoginUserForm(),
     }
     return render(request, 'diary/login.html', params)
+
 
 '''
 新規登録
