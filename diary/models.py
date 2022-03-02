828952f3963bf7ac4d4f8083d0d567e7b2449c19
Yuki Miyata <yuki1113.2001@gmail.com>
2022-03-02 13:54:20 +0900

    [upgrade]ユーザーモデルをカスタムユーザーモデルに変更

--- diary/models.py
+++ diary/models.py
@@ -1,10 +1,21 @@
 import email
 from django.db import models
+from django.contrib.auth.models import AbstractUser
 
 # Create your models here.
-class User(models.Model):
-    email = models.EmailField(max_length=100)
-    password = models.CharField(max_length=100)
+class User(AbstractUser):
+    username = models.CharField(max_length=100)#今回は利用しない
+    email = models.EmailField(max_length=100, unique=True)
+    passward = models.CharField(max_length=100)
+    
+    EMAIL_FIELD = 'email'
+    USERNAME_FIELD = 'email'#ログイン時に要求されるユーザー名をメアドに変更
+    REQUIRED_FIELDS = ['username']
+    
+    #Django標準のユーザーと重複利用されるのを防ぐ
+    class Meta(AbstractUser.Meta):
+        swappable = 'AUTH_USER_MODEL'
+    
     def __str__(self):
         return self.email
 
